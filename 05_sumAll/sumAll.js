33d65b09feaaec06e950e82547a92c937b845828
theokyle <121832097+theokyle@users.noreply.github.com>
2024-08-26 07:31:53 -0500

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+if (!Number.isInteger(num1)||!Number.isInteger(num2)||num1 < 0 || num2 < 0){
+    return 'ERROR';
+}
+
+let sum = 0;
+let small;
+let large;
+
+if (num1 < num2){
+    small = num1;
+    large = num2;
+} else {
+    small = num2;
+    large = num1;
+}
+
+for (i=small; i<=large; i++) {
+    sum += i;
+}
+
+return sum;
 };
 
 // Do not edit below this line

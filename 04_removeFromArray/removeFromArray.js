b2b82d39ee40576ee0058b0c6796315f06e11245
theokyle <121832097+theokyle@users.noreply.github.com>
2024-08-26 07:00:16 -0500

    Passed first test with a for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, number) {
+    let newArray = [];
+    for (i=0; i<array.length; i++) {
+        if (array[i] != number) {
+           newArray.push(array[i]);
+        }
+    }
+return newArray;
 };
 
 // Do not edit below this line

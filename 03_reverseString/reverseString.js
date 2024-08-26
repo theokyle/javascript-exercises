bf5652e81a493351ff27a3ce902f1f4f130c84db
theokyle <121832097+theokyle@users.noreply.github.com>
2024-08-26 06:33:41 -0500

    finished exercise 3 using split function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let newString = string.split("").reverse().join("");
+return newString;
 };
 
 // Do not edit below this line

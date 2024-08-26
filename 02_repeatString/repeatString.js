b124f837013809fe9ae571267bf85fdf6dc416cb
theokyle <121832097+theokyle@users.noreply.github.com>
2024-08-25 20:44:21 -0500

    Finished second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
+if (count < 0){
+    return 'ERROR';
+}
 
+    let string = "";   
+    for (i=0; i<count; i++){
+        string += word;
+    }
+return string;
 };
 
 // Do not edit below this line

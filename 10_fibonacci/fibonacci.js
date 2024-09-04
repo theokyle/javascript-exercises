d9f12965b167257deab58016c645fb0c9cbff4b2
theokyle <121832097+theokyle@users.noreply.github.com>
2024-09-04 03:12:29 -0500

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    
+    let value  = Number(number);
 
+    if (value < 0) {
+        return "OOPS";
+    }
+    else if (value === 0){
+        return 0;
+    }
+    else{
+        const array = [1,1];
+        for (i=2; i<value; i++){
+            let nextNum = array[array.length-2] + array[array.length-1];
+            array.push(nextNum);
+        }
+        return array[value-1];
+    }
 };
 
 // Do not edit below this line

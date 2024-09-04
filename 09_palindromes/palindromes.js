0ad0c2c84ef298a0e223dba44909e65fc64e4652
theokyle <121832097+theokyle@users.noreply.github.com>
2024-09-04 02:48:37 -0500

    finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const letter = "abcdefghijklmnopqrstuvwxyz123456789";
+    const cleanString = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => letter.includes(character))
+        .join('');
 
+    const reverseString = cleanString.split('').reverse().join('');
+    if (reverseString === cleanString) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    
 };
 
 // Do not edit below this line
